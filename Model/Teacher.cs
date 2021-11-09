3c27878e8fbaff66766c615b521c4a0e0540d9de
SATAN <frodriguezcarcher@gmail.com>
2021-11-09 19:55:19 -0300

    arregue teacher

--- Model/Teacher.cs
+++ Model/Teacher.cs
@@ -4,21 +4,25 @@ using System.Linq;
 using System.Web;
 
 namespace OnuProto.Model {
-    private string expertise;
 
-    public string Expertise { get => expertise; set => expertise = value; }
-
-    public Teacher(string userEmail, string userName, int userId) : base(userEmail, userName, userId) {
-        }
+     public class Teacher : User{
 
 
-    private Lecture createLecture(string name, string desc, string contents) {
-        Lecture lec = new Lecture();
-        lec.LectureName = name;
-        lec.LectureDescription = desc;
-        lec.Contents = contents;
-        return lec;
-        }
-    public class Teacher {
-        }
+        private string expertise;
+
+        public string Expertise { get => expertise; set => expertise = value; }
+
+        public Teacher(string userEmail, string userName, int userId) : base(userEmail, userName, userId) {
+            }
+
+
+        private Lecture createLecture(string name, string desc, string contents) {
+            Lecture lec = new Lecture();
+            lec.LectureName = name;
+            lec.LectureDescription = desc;
+            lec.Contents = contents;
+            return lec;
+            }
+
     }
+}
