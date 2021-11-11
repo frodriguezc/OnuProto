8d3c37dc0a03f5a81a66603ede55fd5b171e0193
SATAN <frodriguezcarcher@gmail.com>
2021-11-11 19:28:07 -0300

    crud crear student listo

--- Controller/CRUD.cs
+++ Controller/CRUD.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using OnuProto.Model;
 
 namespace OnuProto.Controller
 {
     public class CRUD
     {
-        public static void CreateUser()
+        public static void CreateUser(string email, string name)
         {
+            string userEmail = email;
+            string userName = name;
+            int userId = 2;
 
+            Student student = new Student(userEmail, userName, userId);
+                //public Student(string userEmail, string userName, int userId) : base(userEmail, userName, userId)
         }
     }
 }
